5284783d9864afc7eb761e7bde79e129734a1350
unknown <joe@.(none)>
2009-11-07 23:11:07 -0800

    Wrapped ignore-error around the font changes, because it doesn't really matter.

--- init.el
+++ init.el
@@ -1,16 +1,18 @@
 ;; Joe Schafer's .emacs
 
-;; Use a decent font
-(modify-all-frames-parameters
- '((font . "Consolas 14")
-   (menu-bar-lines . 0)
-   (tool-bar-lines . 0)))
+;; Use a decent font and ignore errors if the font isn't found.
+(ignore-errors
+  (modify-all-frames-parameters
+   '((font . "Consolas 14")
+     (menu-bar-lines . 0)
+     (tool-bar-lines . 0))))
 
 ;; Add the default-directory and all subdirectories to the load path
 (let ((default-directory "~/.emacs.d/"))
   (setq load-path (cons default-directory load-path))
   (normal-top-level-add-subdirs-to-load-path))
 
+;; Start in a reasonable directory
 (setq default-directory "~/")
 
 ;;; Initial Code Load
