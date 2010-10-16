9436b1bb763b568c8bb05b7cadc8f89c1e844d2d
Joe Schafer <joe@jschaf.com>
2010-10-16 17:30:40 -0400

    Removed unused customization files.

--- customizations/custom-python.el
+++
@@ -1,7 +0,0 @@
-
-(defadvice pylint-python-hook (around pylint-python-hook-around activate)
-  "Add `py-mode-map' to `pylint-python-hook' because it is
-missing in the vanilla `python-mode'"
-  (let ((py-mode-map))
-    (setq py-mode-map python-mode-map)
-    ad-do-it))
