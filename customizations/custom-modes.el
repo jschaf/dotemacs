9e612ce6283ab135c42d5094d60651f114035d13
joe <joe@myhost.(none)>
2010-10-16 17:20:42 -0400

    Removed redundant set-key to kill buffer.

--- customizations/custom-modes.el
+++ customizations/custom-modes.el
@@ -28,7 +28,6 @@
 ;; Misc
 (global-set-key "\C-ha" 'apropos)
 (global-set-key (kbd "<f1>") 'menu-bar-mode)
-(global-set-key [(super meta /)] (lambda () (interactive) (kill-buffer nil)))
 (global-set-key (kbd "C-M-/") (lambda () (interactive) (kill-buffer nil)))
 
 ;; Help-mode
