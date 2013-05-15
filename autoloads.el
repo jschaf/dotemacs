5ada6c5cf1b933678eca0670cc2e938903bbec2e
Joe Schafer <joe@jschaf.com>
2013-05-14 20:58:03 -0400

    Add el-get to the load-path.

--- autoloads.el
+++ autoloads.el
@@ -1,4 +1,5 @@
 ;; Require el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil t)
   (url-retrieve
    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
