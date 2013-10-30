26d8fbd2ff9884580a376aa988a6edd179d1fd72
Joe Schafer <joe@jschaf.com>
2013-10-29 21:18:04 -0400

    Let projectile use its default file locations.
    It doesn't work otherwise and I don't really want to figure it out.

--- misc.el
+++ misc.el
@@ -258,11 +258,6 @@
 ;; Emacs server
 (setq-default server-auth-dir "~/.emacs.d/private/server")
 
-;; Projectile
-(setq-default projectile-cache-file "~/.emacs.d/private/projectile.cache"
-              projectile-known-projects-file
-              "~/.emacs.d/private/projectile-bookmarks.eld")
-
 ;; We need to add text before we can edit it.
 (add-to-list 'evil-insert-state-modes 'git-commit-mode)
 
