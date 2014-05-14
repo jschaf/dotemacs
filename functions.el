5bfbeb3e3b17588d8806b17a43e7116c5c9c4c3b
Joe Schafer <joe@jschaf.com>
2014-05-14 01:23:25 -0400

    fix ace-jump-mode

--- functions.el
+++ functions.el
@@ -282,59 +282,57 @@ figuring out how to reload the package."
   ;; ace-jump is actually a series of commands which makes handling
   ;; by evil difficult (and with some other things as well), using
   ;; this macro we let it appear as one.
-  (eval-after-load 'ace-jump-mode
-    '(progn
-       (defmacro evil-enclose-ace-jump (&rest body)
-         `(let ((old-mark (mark))
-                (ace-jump-mode-scope 'window))
-            (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-            (remove-hook 'post-command-hook #'evil-visual-post-command t)
-            (unwind-protect
-                (progn
-                  ,@body
-                  (recursive-edit))
-              (if (evil-visual-state-p)
-                  (progn
-                    (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-                    (add-hook 'post-command-hook #'evil-visual-post-command
-                              nil t)
-                    (set-mark old-mark))
-                (push-mark old-mark)))))
+  (defmacro evil-enclose-ace-jump (&rest body)
+    `(let ((old-mark (mark))
+           (ace-jump-mode-scope 'window))
+       (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
+       (remove-hook 'post-command-hook #'evil-visual-post-command t)
+       (unwind-protect
+           (progn
+             ,@body
+             (recursive-edit))
+         (if (evil-visual-state-p)
+             (progn
+               (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
+               (add-hook 'post-command-hook #'evil-visual-post-command
+                         nil t)
+               (set-mark old-mark))
+           (push-mark old-mark)))))
 
-       (evil-define-motion evil-ace-jump-char-mode (count)
-         :type exclusive
-         (evil-enclose-ace-jump
-          (ace-jump-mode 5)))
+  (evil-define-motion evil-ace-jump-char-mode (count)
+    :type exclusive
+    (evil-enclose-ace-jump
+     (ace-jump-mode 5)))
 
-       (evil-define-motion evil-ace-jump-line-mode (count)
-         :type line
-         (evil-enclose-ace-jump
-          (ace-jump-mode 9)))
+  (evil-define-motion evil-ace-jump-line-mode (count)
+    :type line
+    (evil-enclose-ace-jump
+     (ace-jump-mode 9)))
 
-       (evil-define-motion evil-ace-jump-word-mode (count)
-         :type exclusive
-         (evil-enclose-ace-jump
-          (ace-jump-mode 1)))
+  (evil-define-motion evil-ace-jump-word-mode (count)
+    :type exclusive
+    (evil-enclose-ace-jump
+     (ace-jump-mode 1)))
 
-       (add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
+  (add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
 
-       ;; some proposals for binding:
+  ;; some proposals for binding:
 
-       (define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-line-mode)
-       (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-word-mode)
+  (define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-line-mode)
+  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-word-mode)
 
-       ;; different jumps for different visual modes
-       (defadvice evil-visual-line (before spc-for-line-jump activate)
-         (define-key evil-motion-state-map (kbd "C-SPC")
-           #'evil-ace-jump-line-mode))
+  ;; different jumps for different visual modes
+  (defadvice evil-visual-line (before spc-for-line-jump activate)
+    (define-key evil-motion-state-map (kbd "C-SPC")
+      #'evil-ace-jump-line-mode))
 
-       (defadvice evil-visual-char (before spc-for-char-jump activate)
-         (define-key evil-motion-state-map (kbd "C-SPC")
-           #'evil-ace-jump-char-mode))
+  (defadvice evil-visual-char (before spc-for-char-jump activate)
+    (define-key evil-motion-state-map (kbd "C-SPC")
+      #'evil-ace-jump-char-mode))
 
-       (defadvice evil-visual-block (before spc-for-char-jump activate)
-         (define-key evil-motion-state-map (kbd "C-SPC")
-           #'evil-ace-jump-char-mode))))
+  (defadvice evil-visual-block (before spc-for-char-jump activate)
+    (define-key evil-motion-state-map (kbd "C-SPC")
+      #'evil-ace-jump-char-mode))
 
 
   (add-hook 'Info-mode-hook
