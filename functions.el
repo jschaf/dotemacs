c8b2f647ca223d2367aadbc6682eaefd1dca7fc3
Joe Schafer <joe@jschaf.com>
2014-07-03 19:35:11 -0400

    add my:toggle-identifier-naming-style
    Switches between camelCase and underscore

--- functions.el
+++ functions.el
@@ -331,6 +331,35 @@ Apply ARGS normally."
           (evil-define-key state keymap k d)
           (message "defined state %s in %s of %s:%s" state keymap k d))))))
 
+
+(defun my:toggle-identifier-naming-style ()
+  "Toggles the symbol at point between C-style naming,
+e.g. `hello_world_string', and camel case,
+e.g. `HelloWorldString'."
+  (interactive)
+  (let* ((symbol-pos (bounds-of-thing-at-point 'symbol))
+         case-fold-search symbol-at-point cstyle regexp func)
+    (unless symbol-pos
+      (error "No symbol at point"))
+    (save-excursion
+      (narrow-to-region (car symbol-pos) (cdr symbol-pos))
+      (setq cstyle (string-match-p "_" (buffer-string))
+            regexp (if cstyle "\\(?:\\_<\\|_\\)\\(\\w\\)" "\\([A-Z]\\)")
+            func (if cstyle
+                     'capitalize
+                   (lambda (s)
+                     (concat (if (= (match-beginning 1)
+                                    (car symbol-pos))
+                                 ""
+                               "_")
+                             (downcase s)))))
+      (goto-char (point-min))
+      (while (re-search-forward regexp nil t)
+        (replace-match (funcall func (match-string 1))
+                       t nil))
+      (widen))))
+
+
 (global-set-key "\C-w" 'kill-region-or-backward-kill-word)
 
 ;; Local Variables:
