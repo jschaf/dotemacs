8682364bea251a484ea1cfb6e42478208a84d862
Joe Schafer <joe@jschaf.com>
2014-09-13 14:02:25 -0400

    remove unused python tweaks

--- misc.el
+++ misc.el
@@ -297,18 +297,6 @@ Primarily for use in .dir-locals.el")
 
   (define-key python-mode-map (kbd "C-c C-o") 'my:python-oh-command-map))
 
-;; (after 'python
-;;   (setq python-shell-interpreter "ipython3"
-;;         python-shell-interpreter-args ""
-;;         python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;         python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;         python-shell-completion-setup-code
-;;         "from IPython.core.completerlib import module_completion"
-;;         python-shell-completion-module-string-code
-;;         "';'.join(module_completion('''%s'''))\n"
-;;         python-shell-completion-string-code
-;;         "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
-
 (after 'erc
   (setq erc-hide-list '("JOIN" "PART" "QUIT")))
 
